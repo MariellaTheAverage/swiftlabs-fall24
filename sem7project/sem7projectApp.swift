068ae05f790d23d23925e4a61b5db5d86f1a3c7a
Mariella <grishenk@petrsu.ru>
2024-09-03 12:14:27 +0300

    Initial Commit

---
+++ sem7project/sem7projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  sem7projectApp.swift
+//  sem7project
+//
+//  Created by Mary Grishchenko on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct sem7projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
