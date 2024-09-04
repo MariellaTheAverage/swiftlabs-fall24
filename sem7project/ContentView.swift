7d899b48e3d79711bb82fe595acb84803a9f5692
Mariella <grishenk@petrsu.ru>
2024-09-04 09:34:35 +0300

    Main panel and task 1

--- sem7project/ContentView.swift
+++ sem7project/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        @State var redirectFlag = 0
+        
+        NavigationSplitView {
+            List {
+                NavigationLink {
+                    Task1View()
+                } label: {
+                    Text("Task 1")
+                }
+                NavigationLink {
+                    Task2View()
+                } label: {
+                    Text("Task 2")
+                }
+                NavigationLink {
+                    Task1View()
+                } label: {
+                    Text("Task 3")
+                }
+                NavigationLink {
+                    Task1View()
+                } label: {
+                    Text("Task 4")
+                }
+            }
+            .navigationTitle("Tasks")
+        } detail: {
+            Text("Mobile development project")
         }
-        .padding()
     }
 }
 
